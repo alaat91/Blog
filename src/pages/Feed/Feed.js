b76f3825baae517dd80d6b643414453532a9c1e9
Alaa_Taleb <alaat@student.chalmers.se>
2024-10-23 15:32:03 +0200

    fix update added posts

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -329,12 +329,14 @@ class Feed extends Component {
         };
         this.setState((prevState) => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               (p) => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts++;
             if (prevState.posts.length >= 2) {
               updatedPosts.pop();
             }
@@ -345,6 +347,7 @@ class Feed extends Component {
             isEditing: false,
             editPost: null,
             editLoading: false,
+            totalPosts: updatedTotalPosts,
           };
         });
         this.setState((prevState) => {
